832e2c28c2ec848c57e53d5bccc536a158bef26c
Matheus Bernardes <matheusbnas@gmail.com>
2023-04-02 21:16:47 -0300

    Efetuado cargo de dados e apresntado o dataframe

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import pandas as pd
 import numpy as np
 
 import streamlit as st
+
+def load_data():
+    return pd.read_csv('data/processed/bikes_completed.csv')
+
+if __name__ == '__main__':
+    df = load_data()
+
+    st.dataframe(df)
+
+    
